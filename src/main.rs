5c41d4423c9b14b1c715d92b428b40a3b01b2628
nobkz <tone.nobukazu@gmail.com>
2022-12-08 19:05:38 +0900

    add image dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use image::GenericImageView;
+
 fn main() {
-    println!("Hello, world!");
+
+    let img = image::open("Thumbsup1.png").unwrap();
+
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", img.dimensions());
+
+    // The color method returns the image's `ColorType`.
+    println!("{:?}", img.color());
 }
